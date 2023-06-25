6187b8ecda9dd26f3b65576ef0918ec2700f5bd7
Mahmoud Elbehery <mahmoudramy84@gmail.com>
2023-06-25 23:43:04 +0300

    Updating 0-bubble_sort.c task solution

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -24,21 +24,21 @@ void swap_ints(int *a, int *b)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, len = size;
-	bool bubbly = false;
+	int bubbly = 1;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (bubbly == false)
+	while (bubbly == 1)
 	{
-		bubbly = true;
+		bubbly = 0;
 		for (i = 0; i < len - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
 				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
-				bubbly = false;
+				bubbly = 1;
 			}
 		}
 		len--;
