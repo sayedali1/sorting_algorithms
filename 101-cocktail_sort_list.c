83319d17c619a2b84b84371d084d79a886e6b922
sayedali1 <said.ali99859@gmail.com>
2023-06-26 17:13:35 +0200

    task6

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,11 +1,12 @@
 #include "sort.h"
+
 /**
  * swapme - swap the nodes themselves.
  * @current: pointer.
  * @current_old: pointer.
  * @list: doubly linked list
  */
-void swap_nodes(listint_t **list, listint_t *current, listint_t *current_old)
+void swapme(listint_t *current, listint_t *current_old, listint_t **list)
 {
 	listint_t *temp1 = current->next;
 	listint_t *temp2 = current_old->prev;
@@ -24,40 +25,36 @@ void swap_nodes(listint_t **list, listint_t *current, listint_t *current_old)
 }
 
 /**
- * cocktail_sort_list - fun that sort num using coctail sorting algorthim
- * @list: pointer to the list we want to sort
+ * cocktail_sort_list - cocktail_sort_list
+ *
+ * @list: doubly linked list
  */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *step, *first = NULL, *last = NULL;
+	listint_t *check = *list, *first = NULL, *last = NULL;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (!list)
+		return;
+	if (!(*list))
+		return;
+	if (!(*list)->next)
 		return;
 	do {
-
-		for (step = *list; step->next != NULL;)
+		while (check->next)
 		{
-			if (step->n > (step->next)->n)
-			{
-				swap_nodes(list, step->next, step);
-				print_list(*list);
-			}
+			if (check->n > check->next->n)
+				swapme(check->next, check, list);
 			else
-			{
-				step = step->next;
-			}
+				check = check->next;
 		}
-		last = step;
-		while (step->prev != first)
+		last = check;
+		while (check->prev != first)
 		{
-			if (step->n < (step->prev)->n)
-			{
-				swap_nodes(list, step, step->prev);
-				print_list(*list);
-			}
+			if (check->n < check->prev->n)
+				swapme(check, check->prev, list);
 			else
-				step = step->prev;
+				check = check->prev;
 		}
-		first = step;
+		first = check;
 	} while (first != last);
 }
