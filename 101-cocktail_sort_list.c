97bacbf4658121d99558a2392215446d8e1cbaa5
sayedali1 <said.ali99859@gmail.com>
2023-06-26 17:25:43 +0200

    task6

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -10,6 +10,9 @@ void swap_nodes(listint_t **list, listint_t *current, listint_t *current_old)
 	listint_t *temp1 = current->next;
 	listint_t *temp2 = current_old->prev;
 
+	if (current == NULL || current_old == NULL)
+		return;
+
 	if (temp1 != NULL)
 		temp1->prev = current_old;
 	if (temp2 != NULL)
@@ -29,30 +32,24 @@ void swap_nodes(listint_t **list, listint_t *current, listint_t *current_old)
  */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *step = (*list)->next, *first = NULL, *last = NULL;
+	listint_t *step = *list, *first = NULL, *last = NULL;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 	do {
-
 		while (step->next != NULL)
 		{
 			if (step->n > (step->next)->n)
-			{
 				swap_nodes(list, step->next, step);
-			}
 			else
-			{
 				step = step->next;
-			}
 		}
 		last = step;
+		
 		while (step->prev != first)
 		{
 			if (step->n < step->prev->n)
-			{
 				swap_nodes(list, step, step->prev);
-			}
 			else
 				step = step->prev;
 		}
